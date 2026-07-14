d0856996d5d075dba066863a31106dc78a8973a7
guzmanpes <gabriel_guzman@snowlineschools.com>
2026-07-13 20:02:33 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB3mwtYzoaRG2EB_eU7NmwjuG3-7TuVgs0",
+  authDomain: "new-draw.firebaseapp.com",
+  databaseURL: "https://new-draw-default-rtdb.firebaseio.com",
+  projectId: "new-draw",
+  storageBucket: "new-draw.firebasestorage.app",
+  messagingSenderId: "462574759617",
+  appId: "1:462574759617:web:0874c2591a54bfcecfae40"
+};
